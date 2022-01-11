f9e2067d2166edff058ff7d7ae777ac05e6abed0
t11s <greenfeatherhelp@gmail.com>
2022-01-10 17:11:05 -0800

    ♻️ Cleanup interface

--- src/interfaces/CERC20.sol
+++ src/interfaces/CERC20.sol
@@ -6,11 +6,17 @@ import {ERC20} from "solmate/tokens/ERC20.sol";
 import {InterestRateModel} from "./InterestRateModel.sol";
 
 abstract contract CERC20 is ERC20 {
+    function mint(uint256) external virtual returns (uint256);
+
+    function borrow(uint256) external virtual returns (uint256);
+
     function underlying() external view virtual returns (ERC20);
 
     function totalBorrows() external view virtual returns (uint256);
 
     function totalFuseFees() external view virtual returns (uint256);
+
+    function repayBorrow(uint256) external virtual returns (uint256);
 
     function totalReserves() external view virtual returns (uint256);
 
@@ -20,29 +26,23 @@ abstract contract CERC20 is ERC20 {
 
     function fuseFeeMantissa() external view virtual returns (uint256);
 
-    function mint(uint256 amount) external virtual returns (uint256);
-
-    function borrow(uint256 amount) external virtual returns (uint256);
-
     function adminFeeMantissa() external view virtual returns (uint256);
 
     function exchangeRateStored() external view virtual returns (uint256);
 
     function accrualBlockNumber() external view virtual returns (uint256);
 
-    function repayBorrow(uint256 amount) external virtual returns (uint256);
+    function redeemUnderlying(uint256) external virtual returns (uint256);
+
+    function balanceOfUnderlying(address) external virtual returns (uint256);
 
     function reserveFactorMantissa() external view virtual returns (uint256);
 
-    function redeemUnderlying(uint256 amount) external virtual returns (uint256);
-
-    function borrowBalanceCurrent(address user) external virtual returns (uint256);
-
-    function repayBorrowBehalf(address, uint256) external virtual returns (uint256);
-
-    function balanceOfUnderlying(address) external virtual returns (uint256);
+    function borrowBalanceCurrent(address) external virtual returns (uint256);
 
     function interestRateModel() external view virtual returns (InterestRateModel);
 
     function initialExchangeRateMantissa() external view virtual returns (uint256);
+
+    function repayBorrowBehalf(address, uint256) external virtual returns (uint256);
 }
