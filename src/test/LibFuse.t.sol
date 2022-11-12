c2f7c6b62e411986bb28a7e6c22ef4b3580db16c
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-11-12 16:56:26 +0100

    test: add test for f6ETH

--- src/test/LibFuse.t.sol
+++ src/test/LibFuse.t.sol
@@ -9,19 +9,29 @@ import {LibFuse} from "../LibFuse.sol";
 
 contract LibFuseTest is DSTestPlus {
     CToken f6DAI = CToken(0x989273ec41274C4227bCB878C2c26fdd3afbE70d);
+    CToken f6ETH = CToken(0xF6551C22276b9Bf62FaD09f6bD6Cad0264b89789);
 
     address f6DAIHolder = 0x81649be6A4f00E3098DA5ff4b166122de4f05cC1;
+    address f6ETHHolder = 0xb48d6C33A96F5519C82569b478fcD723b3A94a2A;
 
     /*///////////////////////////////////////////////////////////////
                            CORRECTNESS TESTS
     //////////////////////////////////////////////////////////////*/
 
-    function testFuseExchangeRateCorrectness() public {
+    function testFuseExchangeRateCorrectnessDai() public {
         assertEq(LibFuse.viewExchangeRate(f6DAI), f6DAI.exchangeRateCurrent());
     }
 
-    function testFuseBalanceOfUnderlyingCorrectness() public {
+    function testFuseBalanceOfUnderlyingCorrectnessDai() public {
         assertEq(LibFuse.viewUnderlyingBalanceOf(f6DAI, f6DAIHolder), f6DAI.balanceOfUnderlying(f6DAIHolder));
+    }
+
+    function testFuseExchangeRateCorrectnessEth() public {
+        assertEq(LibFuse.viewExchangeRate(f6ETH), f6ETH.exchangeRateCurrent());
+    }
+
+    function testFuseBalanceOfUnderlyingCorrectnessEth() public {
+        assertEq(LibFuse.viewUnderlyingBalanceOf(f6ETH, f6ETHHolder), f6ETH.balanceOfUnderlying(f6ETHHolder));
     }
 
     /*///////////////////////////////////////////////////////////////
